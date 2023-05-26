3c7cd1e0b3426cda36f0639e76b134980b2cd74e
Vitor Aoki <vitoraoki17@gmail.com>
2023-05-26 14:57:29 -0300

    requirement 05 - creating product validations and implementing more tests

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,13 +1,8 @@
 import { Request, Response } from 'express';
 import ordersService from '../services/orders.service';
-import mapStatusHTTP from '../utils/mapStatusHTTP';
 
 async function listAllOrders(_req: Request, res: Response) {
-  const serviceResponse = await ordersService.listAllOrders();
-  
-  if (serviceResponse.status !== 'SUCCESSFUL') {
-    return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);  
-  }
+  const serviceResponse = await ordersService.listAllOrders(); 
   
   res.status(200).json(serviceResponse.data);
 }
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -5,14 +5,26 @@ import ProductModel, {
 import { Product } from '../types/Product';
 import { ServiceResponse } from '../types/ServiceResponse';
 
+function verifyIfFieldsExists({
+  name,
+  price,
+}: ProductInputtableTypes): string | null {
+  if (!name) return '"name" is required';
+  if (!price) return '"price" is required';  
+
+  return null;
+}
+
 function validateParams({
   name,
   price,
-  orderId,
 }: ProductInputtableTypes): string | null {
-  if (!name) return 'Name is required';
-  if (!price) return 'Price is required';
-  if (!orderId) return 'orderId is required';
+  // Name validations
+  if (typeof name !== 'string') return '"name" must be a string';
+  if (name.length < 3) return '"name" length must be at least 3 characters long';
+  // Price validations
+  if (typeof price !== 'string') return '"price" must be a string';
+  if (price.length < 3) return '"price" length must be at least 3 characters long'; 
 
   return null;
 }
@@ -21,18 +33,22 @@ async function registerNewProduct(
   product: ProductInputtableTypes,
 ): Promise<ServiceResponse<Product>> {
   let responseService: ServiceResponse<Product>;
-  const error = validateParams(product);
+  const errorContain = verifyIfFieldsExists(product);
+  const errorFormat = validateParams(product);
 
-  if (error) {
-    responseService = { status: 'INVALID_DATA', data: { message: error } };
+  if (errorContain) {
+    responseService = { status: 'INVALID_DATA', data: { message: errorContain } };
+    return responseService;
+  }
+
+  if (errorFormat) {
+    responseService = { status: 'UNPROCESSABLE_ENTITY', data: { message: errorFormat } };
     return responseService;
   }
 
   const newProduct = await ProductModel.create(product);
 
-  responseService = { status: 'SUCCESSFUL', data: newProduct.dataValues };
-
-  return responseService;
+  return { status: 'SUCCESSFUL', data: newProduct.dataValues };
 }
 
 async function listAllProducts(): Promise<ServiceResponse<ProductSequelizeModel[]>> {
